ce4dc34edfe1ce349d3289233e0ab7943a9adfe1
IngRoy <inggarciarodrigoart@gmail.com>
2025-11-04 17:28:29 +0000

    Agregar carga de variables de entorno y dependencias necesarias para el bot de Telegram

--- config.py
+++ config.py
@@ -1,3 +1,8 @@
-SUPABASE_URL=""
-SUPABASE_KEY=""
-BOT_TOKEN=""
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+SUPABASE_URL= os.getenv("SUPABASE_URL")
+SUPABASE_KEY=os.getenv("SUPABASE_KEY")
+BOT_TOKEN=os.getenv("BOT_TOKEN")
